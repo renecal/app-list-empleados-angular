ddd3d1667a670dff0a743cb519204e68d284a467
renecal <rene.calfin@gmail.com>
2022-06-09 21:10:37 -0400

    creando datos de prueba y metodos para contar

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/Empleado';
 
 @Component({
   selector: 'app-empleado-list',
@@ -6,11 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado-list.component.css']
 })
 export class EmpleadoListComponent implements OnInit {
+  // 1. se crea un array del tipo empleado
+  // 2.-
+  listEmpleados: Empleado[] = [
+    {legajo:1, nombre: 'Juan', apellido: 'Pérez', sexo: 'Masculino', salario: 250000},
+    {legajo:2, nombre: 'Marcos', apellido: 'Gonzalez', sexo: 'Masculino', salario: 65000},
+    {legajo:3, nombre: 'Marta', apellido: 'Garcia', sexo: 'Femenino', salario: 75000},
+  ];
 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  countEmpleados():number{
+    return this.listEmpleados.length;
+  }
+
+  countEmpleadosFem():number{
+    return this.listEmpleados.filter(list =>list.sexo === 'Femenino').length;
+  }
+
+  countEmpleadosMasc():number{
+    return this.listEmpleados.filter(list =>list.sexo === 'Masculino').length;
   }
 
 }
